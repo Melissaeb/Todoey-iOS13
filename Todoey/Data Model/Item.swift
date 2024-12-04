fe6c86b28f468ed2ac1632c19209c19e7b316a03
Melissa Elliston-Boyes <melissa.elliston-boyes@trademe.co.nz>
2024-12-04 17:48:28 +1300

    Add check functionality and refactor

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Melissa Elliston-Boyes on 04/12/2024.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
